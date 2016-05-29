eb6e7efb444c1f38eaf6dd8c7435a2c66a46bd81
ianseyer <ian.seyer@gmail.com>
2016-05-29 03:41:54 -0500

    adding in ssl port

--- html/graph.js
+++ html/graph.js
@@ -8,7 +8,7 @@ app.controller('graph', ['$scope', '$http', 'APIURL', function($scope, $http, AP
     "query":"MATCH (n:Article {lowerTitle: {title}}) RETURN n",
     "params":{"title":"quantum mechanics"}
   }).then(function(one){
-    console.log(one);
+    console.log(one.data.id);
     $http.post(APIURL+'cypher', {
       "query":"MATCH (n:Article {lowerTitle: {title}}) RETURN n",
       "params":{"title":"poland"}
